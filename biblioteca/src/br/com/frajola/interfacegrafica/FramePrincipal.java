80b37be3f5ce45fc60ee8d24ea2838de1d8f1c74
PedroMateusCunha <pedrokunhapimenta1234@gmail.com>
2020-01-19 09:50:30 -0300

    Adição de um teste de camadas

--- biblioteca/src/br/com/frajola/interfacegrafica/FramePrincipal.java
+++ biblioteca/src/br/com/frajola/interfacegrafica/FramePrincipal.java
@@ -1,7 +1,8 @@
 package br.com.frajola.interfacegrafica;
 
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import java.awt.Dimension;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -10,35 +11,81 @@ import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.JButton;
 
-public class FramePrincipal implements ActionListener{
-	JLayeredPane jlpPrincipal;
-	JLabel lblNome;
-	JTextField txtNome;
+
+public class FramePrincipal extends JFrame implements ActionListener{
+	private JLayeredPane jlpPrincipal;
+	private JFrame jFrame;
+	private JPanel pnlPainel, pnlOpcoes, pnlAdicionar;
+	private JLabel lblTitulo;
+	private JTextField txtTitulo;
+	private JButton btnAdicionar, btnRemover, btnEditar, btnConsultar;
 
 	public FramePrincipal() {
-		JFrame jFrame = new JFrame("Biblioteca");
-		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		jFrame.setPreferredSize(new Dimension(300, 400));
-		jlpPrincipal = new JLayeredPane();
 		
-		//#Criação do Painel#//
-		JPanel pnlPainel = new JPanel();
-		pnlPainel.setLayout(new GridLayout(9, 1));
+		//#Criação do Frame Inicial#//
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setPreferredSize(new Dimension(600, 500));
+		setLocationRelativeTo(null);
+		
+		//#Criação do Painel Inicial#//
+		pnlPainel = new JPanel(new CardLayout());
+		//pnlPainel.setLayout(new GridLayout(3, 5, 10, 70));
+		
+		//#Criação e Adição do Painel de Camadas#/
+		jlpPrincipal = new JLayeredPane();
+		pnlPainel.add(jlpPrincipal, BorderLayout.CENTER);
+		jlpPrincipal.setLayout(null);
 		
 		//#Adição de Componentes#//
-		lblNome = new JLabel("Nome");
-		pnlPainel.add(lblNome);
-		txtNome = new JTextField();
-		pnlPainel.add(txtNome);
+		JPanel painel = new JPanel();
+		jlpPrincipal.setLayer(painel, 0);
+		painel.setBounds(82, 65, 900, 900);
+		JLabel texto = new JLabel("Ai dento");
+		painel.add(texto);
+		jlpPrincipal.add(painel);
 
-		jFrame.add(jlpPrincipal);
+		JPanel painel1 = new JPanel();
+		jlpPrincipal.setLayer(painel1, 1);
+		painel1.setBounds(82, 65, 315, 132);
+		JLabel texto1 = new JLabel("Ai Fora");
+		painel1.add(texto1);
+		jlpPrincipal.add(painel1);
+		
+		btnAdicionar = new JButton("Adicionar");
+		btnAdicionar.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				pnlAdicionar = new JPanel();
+				painel1.setVisible(false);
+				
+			}
+		});
+		
+		btnRemover = new JButton("Remover");
+		btnEditar = new JButton("Editar");
+		btnConsultar = new JButton("Consultar");
+		
+		pnlPainel.add(painel);
+		pnlPainel.add(painel1);
+		
+		pnlPainel.add(btnAdicionar);
+		pnlPainel.add(btnRemover);
+		pnlPainel.add(btnEditar);
+		pnlPainel.add(btnConsultar);
+
+		getContentPane().add(jlpPrincipal, BorderLayout.CENTER);
 		
 		//#Visualização da interface#//
+		painel.setVisible(false);
 		pnlPainel.setOpaque(true);
-		jFrame.setContentPane(pnlPainel);
-		jFrame.pack();
-		jFrame.setVisible(true);
+		setContentPane(pnlPainel);
+		
+			
+		pack();
+		setVisible(false);
 
 	}
 	
